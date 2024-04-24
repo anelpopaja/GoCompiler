290a8ccd5e686dbccc68c1f035da5a0e2a44a828
anelpopaja <65506594+anelpopaja@users.noreply.github.com>
2024-04-24 13:55:40 +0200

    Add files via upload

---
+++ GoLanguage/chapter09/example1.rs
@@ -0,0 +1,9 @@
+func fib(x int){
+    if x < 3.0 {
+        1+0;
+    }else{
+        fib(x-1) + fib(x-2);
+    };
+}
+  
+fib(10.0);
---
+++ GoLanguage/chapter09/example2.rs
@@ -0,0 +1,5 @@
+import putchard(c float64);
+
+for i in 0...5 {
+    putchard(42);
+}
