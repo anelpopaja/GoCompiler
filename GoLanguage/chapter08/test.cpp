6069cf70e84ee9f13e3a0adc6419766a651da34e
anelpopaja <65506594+anelpopaja@users.noreply.github.com>
2024-04-24 13:56:39 +0200

    Delete GoLanguage directory

--- GoLanguage/chapter08/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern "C" {
-    double average(double, double);
-}
-
-int main() {
-    std::cout << "average of 3.0 and 27.0: " << average(3.0, 27.0) << std::endl;
-}
--- GoLanguage/chapter09/example2.rs
+++
@@ -1,5 +0,0 @@
-import putchard(c float64);
-
-for i in 0...5 {
-    putchard(42);
-}
